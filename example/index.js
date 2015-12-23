1c52a59dc7a1196873744ac7d3d84df1777d1f00
Sean McGary <sean@seanmcgary.com>
2015-12-23 11:59:56 -0500

    super simple implementation

---
+++ example/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by seanmcgary on 12/23/15.
+ */
+'use strict';
+
+let Request = require('../');
+
+Request({
+	method: 'GET',
+	url: 'https://google.com'
+})
+.then((data) => {
+	console.log(data);
+});
